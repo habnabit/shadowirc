8c184fd82cd527757217c590fcc1100e8e03f2bc
dshadow <>
2001-11-20 22:25:41 +0000

    1.1 release version

--- shadowirc/src/version.c
+++ shadowirc/src/version.c
@@ -19,8 +19,8 @@
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-unsigned char myVersion[] = "\p1.1b3r3";
-#define SIVersion 0x01010046
+unsigned char myVersion[] = "\p1.1";
+#define SIVersion 0x01010080
 
 #include "IRCGlobals.h"
 #include "utils.h"
--- shadowirc/src/version.r
+++ shadowirc/src/version.r
@@ -2,15 +2,15 @@
 
 resource 'vers' (1)
 {
-	0x1, 0x10, beta, 0x03,
+	0x1, 0x10, release, 0x00,
 	0,
 	"",
-	"1.1b3"
+	"1.1"
 };
 
 resource 'vers' (2)
 {
-	0x1, 0x10, beta, 0x03,
+	0x1, 0x10, release, 0x00,
 	0,
 	"",
 	"©Ê1996-2001 John Bafford"
