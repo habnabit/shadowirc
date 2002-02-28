927a76dd7987b64c4f262f6323bb11e5022bde7b
dshadow <>
2002-02-28 00:20:04 +0000

    Removed unused #define.

--- shadowirc/src/InputLine.h
+++ shadowirc/src/InputLine.h
@@ -1,6 +1,6 @@
 /*
 	ShadowIRC - A Mac OS IRC Client
-	Copyright (C) 1996-2000 John Bafford
+	Copyright (C) 1996-2002 John Bafford
 	dshadow@shadowirc.com
 	http://www.shadowirc.com
 
@@ -22,7 +22,6 @@
 #ifndef _InputLine_
 #define _InputLine_
 
-#define MAXLINE 500
 #define MAXHIST 4096
 
 #ifndef __INPUTLINE__
